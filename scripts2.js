9115fb7c26de7eeed2094721d6e268c1c33b4da2
Joe <beatle72298@gmail.com>
2019-04-02 09:10:20 -0500

    added shrinking header

--- scripts2.js
+++ scripts2.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > nav - 100) {
 	    $("#header-bar").addClass("changeColor");
+        $("#header-bar").css("padding", "0")
 	  }
 
 	  else{
-		  $("#header-bar").removeClass("changeColor");  	
+		  $("#header-bar").removeClass("changeColor");
+          $("#header-bar").css("padding", "1.5rem 2rem");
 	  }
   })
 })
