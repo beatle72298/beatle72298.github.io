6073d17946bae39187702c16c07e56fed95f6748
Joe <beatle72298@gmail.com>
2019-04-04 09:55:08 -0500

    Cleaned javascript

--- scripts.js
+++ scripts.js
@@ -10,14 +10,47 @@ $(document).ready(function() {
 			    // otherwise change it back to relative
 			    if (scrollTop > stickyNavTop - 100){ 
 			        $('.navbar').addClass('sticky');
+                    $("#header-bar").addClass("changeColor");
+                    $("#header-bar").css("padding", "0");
 			    } else {
 			        $('.navbar').removeClass('sticky');
+                    $("#header-bar").removeClass("changeColor");
+                    $("#header-bar").css("padding", "1.5rem 2rem");
 			    }
 			};
-
 			stickyNav();
 			// and run it again every time you scroll
-			$(window).scroll(function() {
-				stickyNav();
-			});
-        });
+    $(window).scroll(function() {
+        stickyNav();
+    })    
+})
+
+
+/*---------------LIGHTBOX--------------------*/
+$(document).ready(function() {
+// Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+});
--- scripts2.js
+++ scripts2.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var nav = $('.navbar').offset().top;
-  $(window).scroll(function(){
+  $(window).scroll(function() {
   	var scroll = $(window).scrollTop();
 	  if (scroll > nav - 100) {
 	    $("#header-bar").addClass("changeColor");
@@ -12,4 +12,3 @@ $(document).ready(function(){
           $("#header-bar").css("padding", "1.5rem 2rem");
 	  }
   })
-})
